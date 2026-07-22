416b2d3b8d55b5c6db5539296c09d5b03bec35b0
edgarmartinez769 <kh8pm4nvcm@privaterelay.appleid.com>
2026-07-22 00:04:34 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { Client, GatewayIntentBits } = require("discord.js");
+const { Client, GatewayIntentBits, Collection } = require("discord.js");
 
 const client = new Client({
   intents: [
@@ -6,8 +6,27 @@ const client = new Client({
   ]
 });
 
+client.commands = new Collection();
+
+const pingCommand = require("./commands/ping.js");
+client.commands.set(pingCommand.data.name, pingCommand);
+
 client.once("ready", () => {
   console.log(`DT Manager conectado como ${client.user.tag} ⚽`);
+});
+
+client.on("interactionCreate", async interaction => {
+  if (!interaction.isChatInputCommand()) return;
+
+  const command = client.commands.get(interaction.commandName);
+
+  if (!command) return;
+
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+  }
 });
 
 client.login(process.env.TOKEN);
