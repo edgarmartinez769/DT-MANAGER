fa0a784d7e7288a0a7ccddb17cd9ca827e82a4d0
edgarmartinez769 <kh8pm4nvcm@privaterelay.appleid.com>
2026-07-21 23:53:03 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("DT Manager iniciado ⚽");
+const { Client, GatewayIntentBits } = require("discord.js");
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds
+  ]
+});
+
+client.once("ready", () => {
+  console.log(`DT Manager conectado como ${client.user.tag} ⚽`);
+});
+
+client.login(process.env.TOKEN);
