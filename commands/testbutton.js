f21e2ea895e0644b516fd30eb653eb6c31f06710
edgarmartinez769 <kh8pm4nvcm@privaterelay.appleid.com>
2026-07-22 00:41:04 -0600

    Update testbutton.js

--- commands/testbutton.js
+++ commands/testbutton.js
@@ -1,1 +1,23 @@
+const { SlashCommandBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require("discord.js");
 
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("testbutton")
+        .setDescription("Prueba los botones de DT Manager"),
+
+    async execute(interaction) {
+
+        const button = new ButtonBuilder()
+            .setCustomId("test_button")
+            .setLabel("Presiona aquí ⚽")
+            .setStyle(ButtonStyle.Primary);
+
+        const row = new ActionRowBuilder()
+            .addComponents(button);
+
+        await interaction.reply({
+            content: "Prueba de botones de DT Manager 🔥",
+            components: [row]
+        });
+    },
+};
