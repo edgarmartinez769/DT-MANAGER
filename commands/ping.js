2ab31f6b15d006ff92d954506ea7c3ccef329326
edgarmartinez769 <kh8pm4nvcm@privaterelay.appleid.com>
2026-07-22 00:03:16 -0600

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,1 +1,11 @@
+const { SlashCommandBuilder } = require("discord.js");
 
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("ping")
+        .setDescription("Comprueba si DT Manager funciona"),
+
+    async execute(interaction) {
+        await interaction.reply("🏓 Pong!\nDT Manager está funcionando ⚽");
+    },
+};
