c5924a4da172514d439dcf0b80e71af0d4d3ae50
edgarmartinez769 <kh8pm4nvcm@privaterelay.appleid.com>
2026-07-22 08:24:06 -0600

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -3,7 +3,7 @@ const { SlashCommandBuilder } = require("discord.js");
 module.exports = {
     data: new SlashCommandBuilder()
         .setName("ping")
-        .setDescription("Comprueba si DT Manager funciona"),
+        .setDescription("Comprueba si Tactify funciona"),
 
     async execute(interaction) {
         await interaction.reply("🏓 Pong!\nDT Manager está funcionando ⚽");
